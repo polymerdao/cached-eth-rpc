e26dc240f472609e4624f963c27208d43fef7967
Inkvi <alex@polymerlabs.org>
2024-08-21 11:48:23 -0700

    Add PEPTIDE_RPC variable check and endpoint in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Check if required environment variables are set
-if [ -z "$ETH_SEPOLIA_RPC" ] || [ -z "$OP_SEPOLIA_RPC" ] || [ -z "$BASE_SEPOLIA_RPC" ]; then
+if [ -z "$ETH_SEPOLIA_RPC" ] || [ -z "$OP_SEPOLIA_RPC" ] || [ -z "$BASE_SEPOLIA_RPC" ] || [ -z "$PEPTIDE_RPC" ]; then
   echo "Error: One or more required environment variables are not set."
-  echo "Please set ETH_SEPOLIA_RPC, OP_SEPOLIA_RPC, and BASE_SEPOLIA_RPC."
+  echo "Please set ETH_SEPOLIA_RPC, OP_SEPOLIA_RPC, BASE_SEPOLIA_RPC, and PEPTIDE_RPC."
   exit 1
 fi
 
@@ -15,4 +15,5 @@ exec /app/cached-eth-rpc \
   --endpoint=eth-sepolia="$ETH_SEPOLIA_RPC" \
   --endpoint=op-sepolia="$OP_SEPOLIA_RPC" \
   --endpoint=base-sepolia="$BASE_SEPOLIA_RPC" \
+  --endpoint=peptide="$PEPTIDE_RPC" \
  "$@"
